41d3895c505312648c7dfeee84f95d5bdffdf427
Duch-Nuon <61901430+Duch-Nuon@users.noreply.github.com>
2021-01-02 14:37:20 +0700

    follow test

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(AppIndex());
+
+class AppIndex extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: Column(),
+    );
+  }
+}
